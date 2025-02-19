947ae3016a0fd6760338f24a2e6cf26dd5eb116e
Lemesle Cyriaque <100539151+Cyriiaque@users.noreply.github.com>
2025-02-19 14:47:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -102,13 +102,13 @@ AFRAME.registerComponent("click-grab", {
     });
 
     scene.addEventListener("mouseup", function (event) {
-      if ((event.button == 0) & isGrabbed) {
+      if (event.button == 0 && isGrabbed) {
         clickSound.components.sound.playSound();
         isGrabbed = false;
         launcher.removeAttribute("static-body");
         el.setAttribute("dynamic-body", "mass: 1");
       }
-      if ((event.button == 2) & isGrabbed) {
+      if (event.button == 2 && isGrabbed) {
         releaseSound.components.sound.playSound();
         isGrabbed = false;
         el.setAttribute("dynamic-body", "mass: 1");
@@ -117,48 +117,62 @@ AFRAME.registerComponent("click-grab", {
         }, 100);
       }
     });
+  },
+});
 
-    let rightController = document.querySelector("#rightController");
-    if (rightController) {
-      rightController.addEventListener("triggerdown", function () {
-        isGrabbed = true;
-        updatePosition;
-        el.setAttribute("dynamic-body", "mass: 0");
-        camera.addEventListener("componentchanged", (event) => {
-          if (event.detail.name === "rotation") {
-            updatePosition();
-          }
-        });
-        launcher.setAttribute("static-body", "");
-      });
+AFRAME.registerComponent("VR-grab", {
+  init: function () {
+    let el = this.el;
+    let scene = el.sceneEl;
+    let isGrabbed = false;
 
-      rightController.addEventListener("triggerup", function () {
-        if (isGrabbed) {
-          isGrabbed = false;
-          el.setAttribute("dynamic-body", "mass: 1");
-          setTimeout(() => {
-            launcher.removeAttribute("static-body");
-          }, 100);
-        }
-      });
+    function updatePosition(event) {
+      if (isGrabbed) {
+        let controller = event.target;
+        let controllerPos = new THREE.Vector3();
+        let controllerQuat = new THREE.Quaternion();
+
+        controller.object3D.getWorldPosition(controllerPos);
+        controller.object3D.getWorldQuaternion(controllerQuat);
+
+        let offset = new THREE.Vector3(0, 0, -1.5);
+        offset.applyQuaternion(cameraQuat);
+
+        let newPosition = controllerPos.clone().add(offset);
+        el.object3D.position.copy(newPosition);
+      }
     }
-    let leftController = document.querySelector("#leftController");
-    if (leftController) {
-      leftController.addEventListener("triggerdown", function () {
-        isGrabbed = true;
-        updatePosition;
-        el.setAttribute("dynamic-body", "mass: 0");
-        window.addEventListener("mousemove", updatePosition);
-      });
 
-      leftController.addEventListener("triggerup", function () {
-        if (isGrabbed) {
-          isGrabbed = false;
-          el.setAttribute("dynamic-body", "mass: 1");
-          window.removeEventListener("mousemove", updatePosition);
-        }
-      });
-    }
+    let launcher = document.querySelector("#launcher");
+    const clickSound = document.querySelector("#clickSound");
+    const releaseSound = document.querySelector("#releaseSound");
+
+    el.addEventListener("triggerdown", function () {
+      isGrabbed = true;
+      updatePosition;
+      clickSound.components.sound.playSound();
+      launcher.setAttribute("static-body", "");
+      el.setAttribute("dynamic-body", "mass: 0");
+      window.addEventListener("mousemove", updatePosition);
+    });
+
+    scene.addEventListener("triggerup", function (event) {
+      if (isGrabbed) {
+        // if ((event.button == 0) & isGrabbed) {
+        clickSound.components.sound.playSound();
+        isGrabbed = false;
+        launcher.removeAttribute("static-body");
+        el.setAttribute("dynamic-body", "mass: 1");
+      }
+      // if (isGrabbed) {
+      //   releaseSound.components.sound.playSound();
+      //   isGrabbed = false;
+      //   el.setAttribute("dynamic-body", "mass: 1");
+      //   setTimeout(() => {
+      //     launcher.removeAttribute("static-body");
+      //   }, 100);
+      // }
+    });
   },
 });
 
