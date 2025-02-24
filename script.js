d3053ac65216710ebefe1cbc2d041061f27ee34d
Lemesle Cyriaque <100539151+Cyriiaque@users.noreply.github.com>
2025-02-24 14:23:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -123,47 +123,52 @@ AFRAME.registerComponent("click-grab", {
 AFRAME.registerComponent("VR-grab", {
   init: function () {
     let el = this.el;
+    let scene = el.sceneEl;
     let isGrabbed = false;
-    let controller = null;
 
-    this.onGrabStart = function (evt) {
-      // let raycaster = evt.target.components.raycaster;
-      // if (!raycaster) return;
-      // let intersectedEls = raycaster.intersectedEls;
-      // if (intersectedEls.length === 0 || intersectedEls[0] !== el) return;
-
-      isGrabbed = true;
-      controller = evt.target;
-      el.setAttribute("static-body", "mass: 0");
-      controller.addEventListener("triggerup", this.onGrabEnd);
-    };
-
-    this.onGrabEnd = function () {
+    function updatePosition() {
       if (isGrabbed) {
-        el.setAttribute("static-body", "mass: 1");
-        isGrabbed = false;
-        controller.removeEventListener("triggerup", this.onGrabEnd);
-        controller = null;
-      }
-    };
-
-    this.tick = function () {
-      if (isGrabbed && controller) {
+        let controller = document.querySelector("#rightController");
         let controllerPos = new THREE.Vector3();
         let controllerQuat = new THREE.Quaternion();
 
         controller.object3D.getWorldPosition(controllerPos);
         controller.object3D.getWorldQuaternion(controllerQuat);
 
-        let offset = new THREE.Vector3(0, 0, -2.5);
+        let offset = new THREE.Vector3(0, 0, -1.5);
         offset.applyQuaternion(controllerQuat);
 
         let newPosition = controllerPos.clone().add(offset);
         el.object3D.position.copy(newPosition);
       }
-    };
+    }
 
-    el.sceneEl.addEventListener("triggerdown", this.onGrabStart);
+    let launcher = document.querySelector("#launcher");
+    const clickSound = document.querySelector("#clickSound");
+    const releaseSound = document.querySelector("#releaseSound");
+
+    el.addEventListener("triggerdown", function () {
+      isGrabbed = true;
+      clickSound.components.sound.playSound();
+      launcher.setAttribute("static-body", "");
+      el.setAttribute("dynamic-body", "mass: 0");
+      function loop() {
+        if (isGrabbed) {
+          updatePosition();
+          requestAnimationFrame(loop);
+        }
+      }
+      loop();
+    });
+
+    scene.addEventListener("triggerup", function (event) {
+      if (isGrabbed) {
+        clickSound.components.sound.playSound();
+        isGrabbed = false;
+        launcher.removeAttribute("static-body");
+        el.setAttribute("dynamic-body", "mass: 1");
+      }
+    });
   },
 });
 
