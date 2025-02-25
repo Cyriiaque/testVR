58a0de7016f5313273a78d9013b742fd86e95a29
Lemesle Cyriaque <100539151+Cyriiaque@users.noreply.github.com>
2025-02-25 17:23:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-AFRAME.registerComponent("test", {
+AFRAME.registerComponent("testVR", {
   init: function () {
     let el = this.el;
     let scene = el.sceneEl;
     let isGrabbed = false;
 
     // Add event listener for triggerdown event
-    el.addEventListener('triggerdown', function () {
-      el.setAttribute('color', 'red'); // Change color to red
+    el.addEventListener("triggerdown", function () {
+      el.setAttribute("color", "red"); // Change color to red
     });
 
     // Add event listener for triggerup event to reset color
-    el.addEventListener('triggerup', function () {
-      el.setAttribute('color', 'blue'); // Change color back to blue
+    el.addEventListener("triggerup", function () {
+      el.setAttribute("color", "blue"); // Change color back to blue
     });
   },
 });
