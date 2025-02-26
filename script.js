b1e0ce1f27be2769443aca98fe6fa2b572bc06e5
Lemesle Cyriaque <100539151+Cyriiaque@users.noreply.github.com>
2025-02-26 15:31:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const scene = document.querySelector("a-scene");
+const entity = document.createElement("a-entity");
+entity.setAttribute("gltf-model", "#chair");
+entity.setAttribute("position", "0 1 0");
+entity.setAttribute("scale", "0.1 0.08 0.1");
+entity.setAttribute("dynamic-body", "mass: 1;");
+entity.setAttribute("click-grab", "");
+entity.setAttribute("move-objects", "");
+entity.setAttribute("VR-grab", "");
+entity.setAttribute("id", `${Date.now()}`);
+entity.setAttribute("class", "collidable");
+scene.appendChild(entity);
+
 AFRAME.registerComponent("VR-grab", {
   init: function () {
     let el = this.el;
@@ -42,63 +55,6 @@ AFRAME.registerComponent("VR-grab", {
     };
 
     el.sceneEl.addEventListener('triggerdown', this.onGrabStart);
-  }
-});
-
-document.querySelector("#drawer1").addEventListener("click", function () {
-  var drawer1 = document.querySelector("#drawer1");
-  if (
-    drawer1.getAttribute("position").x == -2.6 &&
-    drawer1.getAttribute("position").y == 0 &&
-    drawer1.getAttribute("position").z == -5
-  ) {
-    drawer1.setAttribute(
-      "animation",
-      "property: position; to: -2.6 0 -4.35; dur: 1000; easing: linear"
-    );
-  } else {
-    drawer1.setAttribute(
-      "animation",
-      "property: position; to: -2.6 0 -5; dur: 1000; easing: linear"
-    );
-  }
-});
-
-document.querySelector("#drawer2").addEventListener("click", function () {
-  var drawer2 = document.querySelector("#drawer2");
-  if (
-    drawer2.getAttribute("position").x == -2.6 &&
-    drawer2.getAttribute("position").y == 0.5 &&
-    drawer2.getAttribute("position").z == -5
-  ) {
-    drawer2.setAttribute(
-      "animation",
-      "property: position; to: -2.6 0.5 -4.35; dur: 1000; easing: linear"
-    );
-  } else {
-    drawer2.setAttribute(
-      "animation",
-      "property: position; to: -2.6 0.5 -5; dur: 1000; easing: linear"
-    );
-  }
-});
-
-document.querySelector("#drawer3").addEventListener("click", function () {
-  var drawer3 = document.querySelector("#drawer3");
-  if (
-    drawer3.getAttribute("position").x == -2.6 &&
-    drawer3.getAttribute("position").y == 1 &&
-    drawer3.getAttribute("position").z == -5
-  ) {
-    drawer3.setAttribute(
-      "animation",
-      "property: position; to: -2.6 1 -4.35; dur: 1000; easing: linear"
-    );
-  } else {
-    drawer3.setAttribute(
-      "animation",
-      "property: position; to: -2.6 1 -5; dur: 1000; easing: linear"
-    );
   }
 });
 
